70ddc0c43871a6a4720388c5019679531b241411
Diego Martinez <diego-32-26@hotmail.com>
2022-06-06 22:12:12 -0500

    Estoy aprendiendo a usar funciones avanzadas, asi que hice el reto del Proyecto:filtrando datos de Platzi :D

--- 02 - Data Science/07 - Python Intermedio/curso-python/filtrando_datos.py
+++ 02 - Data Science/07 - Python Intermedio/curso-python/filtrando_datos.py
@@ -73,24 +73,46 @@ DATA = [
 
 
 def run():
-    all_python_devs = [worker["name"] for worker in DATA if worker["language"] == "python"]
-    all_Platzi_workers = [worker["name"] for worker in DATA if worker["organization"] == "Platzi"]
-    adults = list(filter(lambda worker: worker["age"]>18 , DATA))
-    adults = list(map(lambda worker: worker["name"], adults))
-    old_people = list(map(lambda worker: worker |{"old": worker["age"]>70}, DATA ))
 
-    # for worker in all_python_devs:
-    #     print(worker)
+#    all_python_devs = [worker["name"] for worker in DATA if worker["language"] == "python"]
+#    all_Platzi_workers = [worker["name"] for worker in DATA if worker["organization"] == "Platzi"]
+    # adults = list(map(lambda worker: worker["name"], adults))
+    # adults = list(filter(lambda worker: worker["age"]>18 , DATA))
+#     old_people = list(map(lambda worker: worker |{"old": worker["age"]>70}, DATA ))
 
-    # for worker in all_Platzi_workers:
-    #     print(worker)
+#    for worker in all_python_devs:
+#        print(worker)
+
+#    for worker in all_Platzi_workers:
+#        print(worker)
 
     # for worker in adults:
-    #     print (worker)
+        # print (worker)
 
-    for worker in old_people:
-        print (worker)
+#     for worker in old_people:
+#         print (worker)
 
+
+
+# Reto: Hacer lo mismo con funciones invertidas
+
+    # all_python_devs_inv = list(filter(lambda worker: worker["language"] == "python", DATA)) 
+    # all_python_devs_inv = list(map(lambda worker: worker["name"], all_python_devs_inv))
+    # for worker in all_python_devs_inv:
+    #     print(worker)
+
+#    all_Platzi_workers_inv = list(filter(lambda worker: worker["organization"] == "Platzi", DATA))
+#    all_Platzi_workers_inv = list(map(lambda worker: worker["name"], all_Platzi_workers_inv))
+#    for worker in all_Platzi_workers_inv:
+#        print(worker)
+
+    # adults = [worker ["name"] for worker in DATA if worker["age"]>18]
+    # for worker in adults:
+    #     print(worker)
+
+    old_people_inv = [{**worker, **{"old": worker["age"]>70}} for worker in DATA]
+    for worker in old_people_inv:
+        print(worker)
 
 if __name__ == '__main__':
     run()
