535470748c6da981e13464786f5704bd72731d91
Diego Martinez <diego-32-26@hotmail.com>
2022-06-12 11:03:17 -0500

    Versión 1 Hangman

--- 02 - Data Science/Proyectos/01_Hangman/Hangman.py
+++ 02 - Data Science/Proyectos/01_Hangman/Hangman.py
@@ -1,24 +1,52 @@
 import random
+import os
 
-#def Hangman(): 
+def Hangman(): 
  
-words = []
+    words = []
 
-with open("./words.txt", "r", encoding="utf-8") as f:
-    for line in f:
-        words.append(str(line))
-    word = random.choice(words)
-    print(word)
+    with open("./words.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            words.append(str(line))
+        word = random.choice(words)
+        print(word)
 
+    lives = 5
 
+    gap = []
 
-
-
-# def run():
+    gap = ["_" for l in range(len(word)-1)]
     
-# #    Hangman()
+    End_of_game = False
+
+    while not End_of_game:
+        input_letter = input("Enter a letter: ").strip().lower()
+        assert input_letter.isalpha(), "solo puedes ingresar letras"
+        #p=position
+        for p in range(len(word)):
+            letter = word[p]
+            if letter == input_letter:
+                gap[p] = letter
+        if input_letter not in word:
+            lives -=1
+            print(f"You have {lives} lives")
+            if lives == 0:
+                End_of_game = True
+                print("You Lose!")
+        print(f"{' '.join(gap)}")
+
+        if "_" not in gap:
+            os.system("cls")
+            End_of_game = True
+            print("You Win, Congratulation!")
+           
 
 
 
-# if __name__ == '__main__':
-#     run()
+def run():
+    Hangman()
+
+
+
+if __name__ == '__main__':
+    run()
