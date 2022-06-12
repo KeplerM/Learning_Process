09c57e4e45432b3fda955b6c6bb8a07f0d4f976e
Diego Martinez <diego-32-26@hotmail.com>
2022-06-11 21:20:47 -0500

    Inicio versión original de mi juego El Ahorcado

---
+++ 02 - Data Science/07 - Python Intermedio/curso-python/juego_del_ahorcado.py
@@ -0,0 +1,35 @@
+import random
+
+list_word = []
+with open("./archivos/data.txt", "r", encoding="utf-8") as f:
+    for line in f:
+        list_word.append(str(line))
+    #print(list_word)
+    word = random.choice(list_word)
+    print(word)
+
+
+lives = len(word)
+
+d = []
+
+for w in range(len(word)):
+    d += "_"
+
+
+end_of_game = False
+
+while not end_of_game:
+    letter_input = input("Enter a letter: " ).lower()
+    for p in range(len(word)):
+        letter = word[p]
+        if letter == letter:
+            d[p] = letter
+    if letter_input != word:
+        lives -= 1
+        print(f"You have {lives} lives")
+        if lives == 0:
+            end_of_game = True
+            print("You Lose!")
+    print(f"{' '.join(d)}")
+#print(d)
